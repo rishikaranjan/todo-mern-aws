329c777d3f0223595de2a88bb3a1a2fa2b1a60fa
rishikaranjan <48781953+rishikaranjan@users.noreply.github.com>
2023-04-01 14:38:49 +0530

    Add files via upload

---
+++ frontend/src/Screens/ViewScreen.js
@@ -0,0 +1,77 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Store } from "../Store";
+import axios from 'axios';
+import {useNavigate, useParams } from 'react-router-dom';
+import {Helmet} from 'react-helmet-async';
+
+
+export const ViewScreen = () => {
+
+    const navigate = useNavigate();
+
+    const {state} = useContext(Store);
+    const {userInfo} = state;
+
+    const params = useParams();
+    const {id} = params;
+
+
+    const [task, setTask] = useState([]);
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            try {
+                const {data} = await axios.get(
+                    `/api/tasks/${id}`, 
+                    {
+                        headers: {authorization: `Bearer ${userInfo.token}`}
+                    }
+                ); 
+                setTask(data);
+            }
+
+            catch (err) {
+                console.log(err);
+            }
+
+        };
+
+        fetchData();
+
+    }, [userInfo]);
+
+
+
+
+
+    return (
+        <div className="viewScreen">
+        {
+                                        
+            <div>
+
+            <h1>Title</h1>
+            <div>{task.title}</div>
+
+            <h1>Description</h1>
+            <div>{task.description}</div>
+
+            <h1>Due Date</h1>
+            <div>{task.dueDate}</div>
+
+            <h1>Priority</h1>
+            <div>{task.priority}</div>
+
+            <h1>Status</h1>
+            <div>{task.status}</div>
+                    
+            </div>
+                                
+        }
+                           
+        </div>
+    )
+}
